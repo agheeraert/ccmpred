0ec614588e6fc46783a854aa4d37855dd1c073d6
agheeraert <36077088+agheeraert@users.noreply.github.com>
2018-02-22 16:44:11 -0500

    Add files via upload

--- src/Models/LogLossRB.py
+++ src/Models/LogLossRB.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.autograd import Function, Variable
 import torch.nn as nn
@@ -6,7 +7,7 @@ class LogLossRB(nn.Module):
 	'''
 	Negative log loss given position and sequence
 	'''
-	def __init__(self, L, q=22, gpu=False, lambda_h=0.01, lambda_j=0.005 ):
+	def __init__(self, L, q=21, gpu=False, lambda_h=0.01, lambda_j=0.01 ):
 		super(LogLossRB, self).__init__()
 		self.L = L
 		self.q = q
@@ -34,27 +35,39 @@ class LogLossRB(nn.Module):
 			else:
 				Jp = torch.FloatTensor(self.q, self.q, self.L, self.L)
 
-			Jp.copy_(J.data)
+			Jp.copy_(J.view(self.q, self.q, self.L, self.L).data)
 			Jp = torch.transpose(torch.transpose(Jp, dim0=0, dim1=1), dim0=2, dim1=3)
-			Jp = (J.data + Jp)*0.5
-			J.data.copy_(Jp)
+			Jp = (J.view(self.q, self.q, self.L, self.L).data + Jp)*0.5
+		J.data.copy_(Jp.view(self.q*self.q, self.L*self.L))
+	
+	def create_output(self):
+		"""
+		Creates an output file containing the computed H and J
+		"""
+		if not os.path.isdir('../results/'):
+			os.mkdir('../results/')
+
+		torch.save(self.H, '../results/1BDO_A_H.out')
+		torch.save(self.J, '../results/1BDO_A_J.out')	
 	
 	def contact_matrix(self):
 		"""
 		Computes renormalized matrix
 		"""
 		for J in self.J.parameters():
-			Jp = torch.FloatTensor(self.q, self.q, self.L, self.L).copy_(J.data)
-			Jp = Jp - torch.mean(Jp, dim=0) - torch.mean(Jp, dim=1) + torch.mean(torch.mean(Jp, dim=0), dim=0)
-			S_FN = Jp*Jp
-			S_FN = torch.sqrt(torch.sum(torch.sum(S_FN, dim=0), dim=0))
+			Jp = J.data
+		
+		Jp = Jp.view(self.q, self.q, self.L, self.L)
+		Jp = Jp - torch.mean(Jp, dim=0) - torch.mean(Jp, dim=1) + torch.mean(torch.mean(Jp, dim=0), dim=0)
+		S_FN = Jp*Jp
+		S_FN = torch.sqrt(torch.sum(torch.sum(S_FN, dim=0), dim=0))
 
-			S_CN = S_FN - torch.mean(S_FN, dim=0).resize_(1,self.L) * torch.mean(S_FN, dim=1).resize_(self.L,1) / torch.mean(S_FN)
-			return S_CN
+		S_CN = S_FN - torch.mean(S_FN, dim=0).resize_(1,self.L) * torch.mean(S_FN, dim=1).resize_(self.L,1) / torch.mean(S_FN)
+		return S_CN
 
 
-	def forward(self, sigma_r, sigma_i, sigma_ri, r):
-
+	def forward(self, sigma_r, sigma_i, sigma_ri, r, w_b):
+		r = r[0]
 		mask = torch.eye(self.L)
 		if self.gpu:
 			mask = mask.cuda()
@@ -68,21 +81,16 @@ class LogLossRB(nn.Module):
 		for i in range(0,self.q):
 			mask_extended[i, :, :].copy_(mask.data)
 		mask_extended = Variable(mask_extended)
-		
 
-		J_irij = self.J(sigma_i).resize(self.L, self.L, self.L)
-		J_rij = J_irij[:,r,:]
-		J_r = (J_rij*mask).sum()
-		H_r = self.H(sigma_r)
-		H_r = H_r[:,r]
-		nominator = H_r + J_r
+		J_rij = self.J(sigma_i).resize(self.L, self.L, self.L)[:,r,:]
+		nominator = torch.exp(self.H(sigma_r)[0,r] + (J_rij*mask).sum())
 		
 		J_rili = self.J(sigma_ri).resize(self.q, self.L, self.L, self.L)
 		J_ili = torch.squeeze(J_rili[:,:,r,:])
 		J_l = (J_ili*mask_extended).sum(dim=1).sum(dim=1)
-
 		denominator = torch.exp(self.H(self.all_aa)[:,r] + J_l).sum()
-		Lpseudo = -nominator + torch.log(denominator)
+
+		Lpseudo = (-torch.log(nominator) + torch.log(denominator))*w_b[0]
 
 		#regularization
 		for H in self.H.parameters():
@@ -100,9 +108,9 @@ if __name__=='__main__':
 	for i, a in enumerate(aa):
 		aa_to_idx[a] = i
 
-	print aa_to_idx
+	print(aa_to_idx)
 
-	with open("../../database/1atzA.aln", "r") as msa_file:
+	with open("../../database/1BDO_A.aln", "r") as msa_file:
 		msa = []
 		for line in msa_file.readlines():
 			msa.append(list(line.split()[0]))
@@ -126,7 +134,7 @@ if __name__=='__main__':
 
 	loss = LogLossRB(L)
 	for parameter in loss.parameters():
-		print parameter
+		print(parameter)
 
 	y = loss(s_r, s_i, all_aa_si, r)
 	y.backward()
